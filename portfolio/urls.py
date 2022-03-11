77588929bc867eacb6a0fd71322443ffcbf9ddb6
Shihab Uddin <contact.shihab.247@gmail.com>
2022-03-11 21:56:41 +0600

    node branch initializing

--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('theme.urls')),
-    path('api/v1/', include('theme.api_urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
-]
--- theme/api_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .serializers import *
-
-urlpatterns = [
-    path('blog/', BlogSerializer.as_view(), name='blog-list'),
-    path('blog/<int:pk>', BlogDetailSerializer.as_view()),
-]
